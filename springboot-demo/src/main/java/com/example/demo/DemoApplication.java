e6060926f2ebfd754e5eb7c6029103c9298ab844
Dmitriy Dubson <ddubson@pivotal.io>
2017-07-26 17:04:59 -0600

    Added task execution spike

---
+++ springboot-demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public ResponseEntity index() {
+		return ok("Hello world from Demo app.");
+	}
+}
--- src/main/java/com/dmitriydubson/boot/scale/BootScaleTask.java
+++ src/main/java/com/dmitriydubson/boot/scale/BootScaleTask.java
@@ -3,16 +3,40 @@ package com.dmitriydubson.boot.scale;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BootScaleTask extends DefaultTask {
     @TaskAction
     public void run() {
+        BootScalePluginExtension settings = loadPluginSettings();
+        scalingTo(settings.getNumberOfInstances());
+
+        try {
+            Process process = Runtime.getRuntime().exec("gradle bootRun -Dserver.port=9000");
+            try(BufferedReader reader = new BufferedReader (new InputStreamReader(process.getInputStream()))) {
+                String line = null;
+                while((line = reader.readLine()) != null) {
+                    System.out.println("[PROCESS] " + line);
+                }
+            }
+            System.out.println("Process started! Info: " + process.toString());
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private void scalingTo(int instances) {
+        System.out.format("BootScale scaling to %d instances.\n", instances);
+    }
+
+    private BootScalePluginExtension loadPluginSettings() {
         BootScalePluginExtension extension = getProject().getExtensions()
                 .findByType(BootScalePluginExtension.class);
         if (extension == null) {
             extension = new BootScalePluginExtension();
         }
-
-        System.out.format("BootScale scaling to %d instances.\n",
-                extension.getNumberOfInstances());
+        return extension;
     }
 }
